e06c217a3d245b68099fc93d6cd919aa35ccd89c
HeyoItsAaron <heyitsaaronwilliams@gmail.com>
2022-10-26 11:17:07 -0500

    GRASS SHADERS DONE BABY

--- Assets/Valem Video/Scripts/NetworkPlayerSpawner.cs
+++ Assets/Valem Video/Scripts/NetworkPlayerSpawner.cs
@@ -7,6 +7,11 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
     private GameObject spawnedPlayerPrefab;
 
+    public override void OnJoinedLobby() //just added, shoudln't this add player to lobby?
+    {
+        base.OnJoinedLobby();
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+    }
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
