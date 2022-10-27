f38a5f7b0e3283ca08a6d0e733e75c8e36109c00
HeyoItsAaron <heyitsaaronwilliams@gmail.com>
2022-10-27 15:20:26 -0500

    MP MOTHERCLUCKIN' WORKS
    Need To:
    - Make head location taller (set at beginning to head of robot)
    - Make hand animations come through (leg anims do so it's an animator thing)
    - Add a couple guns to the training grounds
    - add Ali's scripts to the player avatars
    - make banana man a player avatar
    - extend kyle's arms?

--- Assets/Valem Video/Scripts/VRRig.cs
+++ Assets/Valem Video/Scripts/VRRig.cs
@@ -26,7 +26,7 @@ public class VRRig : MonoBehaviour
     public VRMap leftHand;
     public VRMap rightHand;
 
-    private Vector3 headBodyOffest;
+    private Vector3 headBodyOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -34,13 +34,13 @@ public class VRRig : MonoBehaviour
         if (!root)
             root = transform;
 
-        headBodyOffest = root.position - head.rigTarget.position;
+        headBodyOffset = root.position - head.rigTarget.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        root.position = head.rigTarget.position + headBodyOffest;
+        root.position = head.rigTarget.position + headBodyOffset;
         root.forward = Vector3.Lerp(root.forward,
         Vector3.ProjectOnPlane(head.vrTarget.forward,Vector3.up).normalized, turnSmoothness);
 
