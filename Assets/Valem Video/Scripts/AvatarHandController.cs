68711aefbe5e55d512237a6e825a6d4e64dcd7c2
HeyoItsAaron <heyitsaaronwilliams@gmail.com>
2022-10-30 17:20:17 -0500

    HAND ANIMATIONS WORKING

--- Assets/Valem Video/Scripts/AvatarHandController.cs
+++ Assets/Valem Video/Scripts/AvatarHandController.cs
@@ -1,28 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
-[RequireComponent(typeof(ActionBasedController))]
 public class AvatarHandController : MonoBehaviour
 {
-    ActionBasedController controller;
-    AvatarHand hand;
 
-    // Start is called before the first frame update
-    void Start()
+    public InputActionProperty leftTriggerPull;
+    public InputActionProperty leftGrabPull;
+    public InputActionProperty leftPointTouch;
+    public InputActionProperty leftThumbTouch;
+
+    public InputActionProperty rightTriggerPull;
+    public InputActionProperty rightGrabPull;
+    public InputActionProperty rightPointTouch;
+    public InputActionProperty rightThumbTouch;
+
+    public Animator avatarAnimator;
+
+    void UpdateLeftHand()
     {
-        //controller = GetComponent<ActionBasedController>();
+        //set pinch
+        float triggerValueLeft = leftTriggerPull.action.ReadValue<float>();
+        avatarAnimator.SetFloat("Left Trigger", triggerValueLeft);
+
+        //set grab
+        float grabValueLeft = leftGrabPull.action.ReadValue<float>();
+        avatarAnimator.SetFloat("Left Grab", grabValueLeft);
+
+        /*
+        //set point
+        bool isPointTouchLeft = System.Convert.ToBoolean(leftPointTouch.action.ReadValue<float>());
+        if (isPointTouchLeft != true)
+        {
+            avatarAnimator.SetLayerWeight(avatarAnimator.GetLayerIndex("Left Point Layer"), 1);
+        }
+        else
+        {
+            avatarAnimator.SetLayerWeight(avatarAnimator.GetLayerIndex("Left Point Layer"), 0);
+        }
+
+        //set thumb
+        bool isThumbTouchLeft = System.Convert.ToBoolean(leftThumbTouch.action.ReadValue<float>());
+        if (isThumbTouchLeft != true)
+        {
+            avatarAnimator.SetLayerWeight(avatarAnimator.GetLayerIndex("Left Thumb Layer"), 1);
+        }
+        else
+        {
+            avatarAnimator.SetLayerWeight(avatarAnimator.GetLayerIndex("Left Thumb Layer"), 0);
+        }
+        */
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateRightHand()
     {
-        //hand.SetGrip(controller.selectAction.action.ReadValue<float>());
-        //hand.SetTrigger(controller.activateAction.action.ReadValue<float>());
-        //hand.SetGripTouch(controller.selectAction.action.ReadValue<bool>());
-        //hand.SetTriggerTouch(controller.activateAction.action.ReadValue<bool>());
-        //controller.TryGetFeatureValue(CommonUsages.trigger, out float triggerPullValue);
+        //Right Hand
+        float triggerValueRight = rightTriggerPull.action.ReadValue<float>();
+        avatarAnimator.SetFloat("Right Trigger", triggerValueRight);
+
+        float grabValueRight = rightGrabPull.action.ReadValue<float>();
+        avatarAnimator.SetFloat("Right Grab", grabValueRight);
+
+        /*
+        bool isPointTouchRight = System.Convert.ToBoolean(rightPointTouch.action.ReadValue<float>());
+        if (isPointTouchRight != true)
+        {
+            avatarAnimator.SetLayerWeight(avatarAnimator.GetLayerIndex("Right Point Layer"), 1);
+        }
+        else
+        {
+            avatarAnimator.SetLayerWeight(avatarAnimator.GetLayerIndex("Right Point Layer"), 0);
+        }
+
+        bool isThumbTouchRight = System.Convert.ToBoolean(rightThumbTouch.action.ReadValue<float>());
+        if (isThumbTouchRight != true)
+        {
+            avatarAnimator.SetLayerWeight(avatarAnimator.GetLayerIndex("Right Thumb Layer"), 1);
+        }
+        else
+        {
+            avatarAnimator.SetLayerWeight(avatarAnimator.GetLayerIndex("Right Thumb Layer"), 0);
+        }
+        */
+    }
+    void FixedUpdate()
+    {
+        UpdateLeftHand();
+        UpdateRightHand();
     }
 }
