1d7f3dc08b2da00c76c8205c2ea1d53419f00633
HeyoItsAaron <heyitsaaronwilliams@gmail.com>
2022-11-16 20:28:17 -0600

    Started adding Wall Guns to buy

---
+++ Assets/WallBuy.cs
@@ -0,0 +1,39 @@
+// Aaron Williams
+// 11/16/2022
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class WallBuy : MonoBehaviour
+{
+    // variables
+    [SerializeField] private GameObject weaponPrefab;
+    [SerializeField] private GameObject weaponSpawnPoint;
+    [SerializeField] private float price;
+    private Player player;
+
+
+    // methods
+    void Start()
+    {
+        player = GameObject.FindObjectOfType<Player>();
+    }
+
+    public void BuyWeapon()
+    {
+        if (player.currCurrency >= price)
+        {
+            player.currCurrency -= price;
+            PhotonNetwork.Instantiate(weaponPrefab.name, weaponSpawnPoint.transform.position, weaponSpawnPoint.transform.rotation);
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
