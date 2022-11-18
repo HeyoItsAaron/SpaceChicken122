5cfe3e4470ba31bb88893451faa3b28110161583
Ali Rashid <ali.rashid24a@gmail.com>
2022-11-18 02:19:02 -0600

    FINAL GUN UPDATE 2

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 using static Unity.VisualScripting.Member;
+using UnityEngine.InputSystem;
 
 public class Weapon : MonoBehaviour
 {
     //Base variables
+
+    public InputActionReference toggleReference = null;
+
     public GameObject bullet;
     public Transform spawnPoint;
     public float fireSpeed;
@@ -18,10 +23,12 @@ public class Weapon : MonoBehaviour
     public bool isShotgun;
     public float spread;
     public int bulletsFired;
+    //private List<Quaternion> bullets;
+
 
     //AutoFire
-    public bool isAuto;
-    public float timeBetweenShots;
+    //public bool isAuto;
+    public float fireRate;
 
     //Audio
     public AudioSource gunshot;
@@ -31,37 +38,68 @@ public class Weapon : MonoBehaviour
     // Effects
     public ParticleSystem muzzleFlash;
 
+    private Coroutine _current;
 
+    public bool isGrabbed;
 
     void Start()
     {
         // Once weapon is grabbed, waits for trigger to be pressed
-        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
-        grabbable.activated.AddListener(FireBullet);
+     //    XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+      //   grabbable.activated.AddListener(FireBullet);
         // Muzzle flash is enabled on start
         muzzleFlash.Stop();
+        isGrabbed = false;
     }
 
     void Update()
     {
-
+        float triggerValue = toggleReference.action.ReadValue<float>();
+        if(triggerValue < 0.5 && isGrabbed == true)
+        {
+            Debug.Log("Stop fire");
+            StopFire();
+        }
+        if(triggerValue > 0.5 && isGrabbed == true)
+        {
+            Debug.Log("Start fire");
+            BeginFire();
+        }    
     }
 
-    void FireBullet(ActivateEventArgs arg)
+    void Grabed(SelectEnterEventArgs arg)
     {
-        if (isAuto == true)
-        {
-            Invoke("Shoot()", timeBetweenShots);
-        }
-        else
-        {
-            Shoot();
-        }
+        //BeginFire();
+        isGrabbed = true;
     }
-
-    void Shoot()
+    void notGrabbed(SelectExitEventArgs arg)
     {
-        //if(ammo > 1) Sample ammo check
+        //BeginFire();
+        isGrabbed = false;
+    }
+    // void StopFireBullet(DeactivateEventArgs arg)
+    // {
+    //     StopFire();
+    //  }
+    void BeginFire()
+    {
+        if (_current != null)
+        {
+            StopCoroutine(_current);
+        }
+        _current = StartCoroutine(Shoot());
+    }
+    void StopFire()
+    {
+        if (_current != null)
+        {
+            StopCoroutine(_current);
+        }
+
+    }
+    private IEnumerator Shoot()
+    {
+        while(true)
         {
             // Makes an object of the audio source to destory to prevent it spawning infinite times
             AudioSource newAS = Instantiate(gunshot);
@@ -71,31 +109,32 @@ public class Weapon : MonoBehaviour
             // Muzzle Flash
             muzzleFlash.Play();
 
-            if (isShotgun == true)
+            if (!isShotgun)
             {
+                Debug.Log("Normal shoot");
+                GameObject spawnedBullet = Instantiate(bullet, spawnPoint.transform.position, spawnPoint.transform.rotation);
+                spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+                Destroy(spawnedBullet, 5);
+            }
+            else
+            {
+                Debug.Log("Shot gun shoot");
                 for (int i = 0; i < bulletsFired; i++)
                 {
                     //Random spread
                     float x = UnityEngine.Random.Range(-spread, spread);
                     float y = UnityEngine.Random.Range(-spread, spread);
-                    Vector3 Direction = spawnPoint.transform.position + new Vector3(x, y, 0);
+
+                    Vector3 direction = spawnPoint.transform.position + new Vector3(x,y,0);
 
                     // Bullet Spawning
-                    GameObject spawnedBullet = Instantiate(bullet, Direction, spawnPoint.transform.rotation);
-                    spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+                    GameObject spawnedBullet = Instantiate(bullet, direction, spawnPoint.transform.rotation);
+                    spawnedBullet.GetComponent<Rigidbody>().velocity = direction * fireSpeed;
                     Destroy(spawnedBullet, 5);
                 }
-
             }
-            else
-            {
-                GameObject spawnedBullet = Instantiate(bullet, spawnPoint.transform.position, spawnPoint.transform.rotation);
-                spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
-                Destroy(spawnedBullet, 5);
-            }
-
-            //ammo = ammo - ammoCost; sample ammo update on shoot
+            yield return new WaitForSeconds(1f / fireRate);
         }
-    }
 
+    }
 }
