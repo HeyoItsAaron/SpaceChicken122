eeaff31bd35cfa099b1d6088221f4c7b13e25fb0
HeyoItsAaron <heyitsaaronwilliams@gmail.com>
2022-11-18 15:52:05 -0600

    Got rid of some errors

--- Assets/Scripts/Aaron/GameplayManager.cs
+++ Assets/Scripts/Aaron/GameplayManager.cs
@@ -17,33 +17,15 @@ public class GameplayManager : MonoBehaviour
     {
         waveNumber = 0;
         spawner = GameObject.FindObjectOfType<testSpawner>();
-
-        if (spawner != null || spawner.spawners != null)
-        {
-            StartWave();
-        }
     }
     void Update()
     {
-        if (isSafe)
-        {
-            if (enemyAmount == 0)
-            {
-                StartWave();
-            }
-        }
-    }
-    void safetyCheck()
-    {
-        if(spawner != null || spawner.spawners !=null)
-        {
-            isSafe = true;
-        }
+        
     }
     public void StartWave()
     {
         waveNumber++;
         spawnThisMany = waveNumber * 2;
-        spawner.SpawnEnemy((waveNumber * 2));
+        //spawner.SpawnEnemy((waveNumber * 2));
     }
 }
