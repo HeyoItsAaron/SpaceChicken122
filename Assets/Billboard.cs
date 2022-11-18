d8ccdbb381807a9caf90ca6b7bf90ca3e668b06a
HeyoItsAaron <heyitsaaronwilliams@gmail.com>
2022-11-18 02:12:16 -0600

    Billboard as an rpc?

--- Assets/Billboard.cs
+++ Assets/Billboard.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Photon.Pun;
 
 public class Billboard : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI WaveNum;
     public NetworkSpawner spawner;
-    
+
     void Start()
     {
         spawner = FindObjectOfType<NetworkSpawner>();
@@ -18,9 +19,10 @@ public class Billboard : MonoBehaviour
         {
             spawner = FindObjectOfType<NetworkSpawner>();
         }
-        LinkWave();
+        gameObject.GetComponent<PhotonView>().RPC("LinkWave", RpcTarget.AllBuffered);
     }
 
+    [PunRPC]
     public void LinkWave()
     {
         WaveNum.text = spawner.waveNumber.ToString();
