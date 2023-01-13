ac45cdd2bb690580ba366887279824f59a44e0a7
HeyoItsAaron <heyitsaaronwilliams@gmail.com>
2023-01-13 14:23:43 -0600

    Revert "Doors and teleport to spawn Scripts"
    This reverts commit 7f74a013c864f6d005cbac0d6be366f496395f9c.

--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportToSpawn : MonoBehaviour
+public class DoorScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindObjectOfType<PlayerStats>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-
-    void OnTriggerEnter(Collider col)
-    {
-        col.Transform.position = new Vector3(-25.33, 1.56, 101.11);
     }
 }
